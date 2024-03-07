4f97ac17366d0b3c414836bf6edd300e06520817
billkiplagat <bill.kiplagat.7@gmail.com>
2024-03-07 11:47:13 +0300

    fixed all lint errors

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,12 +1,11 @@
-function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = (newNeighborhood) => {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
+function getNeighborhoodsList () {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square']
+
+  const self = this
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood)
+    return self.sanFranciscoNeighborhoods
   }
-  
-  export default getNeighborhoodsList;
-  
+}
+
+export default getNeighborhoodsList
